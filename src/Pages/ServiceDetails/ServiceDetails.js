dda5c72b36691486be11363a1dbe231b6af0a237
shahidmonowarr <shahidmonowar21@gmail.com>
2021-10-19 18:10:29 +0600

    service details

--- src/Pages/ServiceDetails/ServiceDetails.js
+++ src/Pages/ServiceDetails/ServiceDetails.js
@@ -3,15 +3,40 @@ import { useParams } from 'react-router';
 
 const ServiceDetails = () => {
     let { serviceId } = useParams();
-    // const [details, setDetails] = useState([])
-    // useEffect(() => {
-    //     fetch('fakedb.json')
-    //         .then(res => res.json())
-    //         .then(data => console.log(data))
-    // }, [])
+    const [details, setDetails] = useState([]);
+
+    const [matchedDetail, setMatchedDetail] = useState({});
+
+    console.log(matchedDetail)
+
+    useEffect(() => {
+        fetch('/fakedb.json')
+            .then(res => res.json())
+            .then(data => setDetails(data))
+    }, [])
+
+    useEffect(() => {
+        const foundEmployee = details.find(detail => detail._id === serviceId)
+        setMatchedDetail(foundEmployee);
+    }, [details]);
+
+
     return (
-        <div>
-            <h2>this is details {serviceId}</h2>
+        <div className="container">
+            {/* <h2>this is details {serviceId}</h2> */}
+            <div className="card mb-3" >
+                <div className="row g-0">
+                    <div className="col-md-4">
+                        <img src={matchedDetail?.picture} className="img-fluid rounded-start" alt="..." />
+                    </div>
+                    <div className="col-md-8">
+                        <div className="card-body">
+                            <h5 className="card-title">{matchedDetail?.name}</h5>
+                            <p className="card-text">{matchedDetail?.description}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
